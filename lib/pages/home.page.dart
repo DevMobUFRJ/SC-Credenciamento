26ff7705e97a15c6a8d75645db851624026565f4
Eduardodsb <eduardo_b_bosa@hotmail.com>
2020-02-07 17:45:42 -0300

    #Homepage

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -61,11 +61,11 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      /*floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),*/ // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
